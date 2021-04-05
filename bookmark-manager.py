9999b632d2e3b8c5787dd954fe23f8d8dc70929d
Manuel Brea <m.brea.carreras@gmail.com>
2021-04-05 20:57:40 +0100

    Finished mostly working protopype

--- bookmark-manager.py
+++ bookmark-manager.py
@@ -6,32 +6,88 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gio, GObject
 
-class Bookmark(GObject.GObject):
-
-    __gsignals__ = {
-            'modified': (GObject.SignalFlags.RUN_FIRST, None, ()),
-    }
-
+class Bookmark:
     def __init__(self, lines):
-        GObject.GObject.__init__(self)
         self.title = lines[0].split(sep=':', maxsplit=1)[1][1:]
         self.level = int(lines[1].split(sep=':', maxsplit=1)[1])
         self.page = int(lines[2].split(sep=':', maxsplit=1)[1])
 
     def set_title(self, new_title):
         self.title = new_title
-        self.emit('modified')
 
     def set_level(self, new_level):
         self.level = new_level
-        self.emit('modified')
 
     def set_page(self, new_page):
         self.page = new_page
-        self.emit('modified')
+
+class BookmarkStore(Gtk.TreeStore):
+    def __init__(self, bookmark_list: list[Bookmark]):
+        Gtk.TreeStore.__init__(self, str, int)
+        prev_of_level = dict()
+        for bookmark in bookmark_list:
+            found = False
+            for lvl in range(bookmark.level - 1, 0, -1):
+                if lvl in prev_of_level:
+                    treeiter = self.append(prev_of_level[lvl],\
+                            (bookmark.title, bookmark.page))
+                    prev_of_level[bookmark.level] = treeiter
+                    found = True
+                    break
+            if not found:
+                treeiter = self.append(None,\
+                        (bookmark.title, bookmark.page))
+                prev_of_level[bookmark.level] = treeiter
+
+    def __row_to_string(self, accumulator, store, treepath, treeiter):
+        row = store[treeiter]
+        level = treepath.get_depth()
+        accumulator.append(f"BookmarkBegin\nBookmarkTitle: {row[0]}\nBookmarkLevel: {level}\nBookmarkPageNumber: {row[1]}")
 
     def to_string(self):
-        return f"BookmarkBegin\nBookmarkTitle: {self.title}\nBookmarkLevel: {self.level}\nBookmarkPageNumber: {self.page}"
+        bookmark_strings = []
+        self.foreach(lambda store, treepath, treeiter: self.__row_to_string(bookmark_strings, store, treepath, treeiter))
+        return reduce(lambda a,b: a + '\n' + b, bookmark_strings, "")
+
+class PdfTk:
+    def __init__(self, filename):
+        self.filename = filename
+        self.metadata_pre = []
+        self.metadata_post = []
+        self.bookmark_store = None
+
+        bookmarks = []
+        temp_data = enumerate(subprocess.check_output(["pdftk", filename, "dump_data"], \
+                stderr=subprocess.STDOUT).splitlines())
+        found_number_of_pages = False
+
+        for _, line_bytes in temp_data:
+            line = line_bytes.decode('UTF-8')
+            if line == "BookmarkBegin":
+                bookmark = [ temp_data.__next__()[1].decode('UTF-8') ]
+                bookmark.append(temp_data.__next__()[1].decode('UTF-8'))
+                bookmark.append(temp_data.__next__()[1].decode('UTF-8'))
+                bookmarks.append(Bookmark(bookmark))
+            else:
+                if found_number_of_pages:
+                    self.metadata_post.append(line)
+                else:
+                    self.metadata_pre.append(line)
+            if line.find("NumberOfPages:") != -1:
+                found_number_of_pages = True
+        self.bookmark_store = BookmarkStore(bookmarks)
+
+    def save_as(self, new_filename):
+        process = subprocess.Popen(["pdftk", self.filename, "update_info_utf8", "-", "output", new_filename],\
+                stdin=subprocess.PIPE)
+        add_str = lambda a,b: a + "\n" + b
+        full_metadata = reduce(add_str, self.metadata_pre, "") + \
+                self.bookmark_store.to_string() + \
+                reduce(add_str, self.metadata_post, "")
+        process.communicate(input=full_metadata.encode('UTF-8'))
+
+
+
 
 class BookmarkListRow(Gtk.ListBoxRow):
     def __init__(self, bookmark: Bookmark):
@@ -46,9 +102,12 @@ class BookmarkListRow(Gtk.ListBoxRow):
         
 
 class BookmarkEditBox(Gtk.Box):
-    def __init__(self, bookmark: Bookmark):
+    def __init__(self, bookmark_store: BookmarkStore, store_view: Gtk.TreeView):
         Gtk.Box.__init__(self, orientation = Gtk.Orientation.VERTICAL)
-        self.bookmark = bookmark
+        self.bookmark_store = bookmark_store
+        self.store_view = store_view
+        self.selection = store_view.get_selection()
+        self.selection.connect("changed", self.on_change_bookmark)
 
         self.empty_label = Gtk.Label(label="No bookmark selected")
 
@@ -58,13 +117,6 @@ class BookmarkEditBox(Gtk.Box):
         self.title_box.pack_start(Gtk.Label(label="Title:"), True, True, 0) 
         self.title_box.pack_start(self.title_entry, True, True, 0)
 
-        self.level_box = Gtk.Box()
-        self.level_entry = Gtk.Entry()
-        self.level_entry.set_input_purpose(Gtk.InputPurpose.NUMBER)
-        self.level_entry.connect("activate", self.on_activate_level)
-        self.level_box.pack_start(Gtk.Label(label="Level:"), True, True, 0) 
-        self.level_box.pack_start(self.level_entry, True, True, 0)
-
         self.page_box = Gtk.Box()
         self.page_entry = Gtk.Entry()
         self.page_entry.set_input_purpose(Gtk.InputPurpose.NUMBER)
@@ -72,78 +124,38 @@ class BookmarkEditBox(Gtk.Box):
         self.page_box.pack_start(Gtk.Label(label="Page number:"), True, True, 0) 
         self.page_box.pack_start(self.page_entry, True, True, 0)
 
-        self.on_change_bookmark()
+        self.add(self.empty_label)
 
         self.show_all()
 
     def clear_children(self):
         for child in self.get_children():
             self.remove(child)
+
+    def set_store(self, new_store):
+        self.bookmark_store = new_store
     
-    def on_change_bookmark(self):
+    def on_change_bookmark(self, selection):
         self.clear_children()
-        if self.bookmark is None:
+        model, treeiter = self.selection.get_selected()
+        if self.bookmark_store is None or self.selection is None:
             self.pack_start(self.empty_label, True, True, 0)
         else:
             self.pack_start(self.title_box, False, False, 0)
-            self.pack_start(self.level_box, False, False, 0)
             self.pack_start(self.page_box, False, False, 0)
 
-            self.title_entry.set_text(self.bookmark.title)
-            self.page_entry.set_text(str(self.bookmark.page))
-            self.level_entry.set_text(str(self.bookmark.level))
+            self.title_entry.set_text(self.bookmark_store[treeiter][0])
+            self.page_entry.set_text(str(self.bookmark_store[treeiter][1]))
 
         self.show_all()
 
-    def change_bookmark(self, new_bookmark):
-        self.bookmark = new_bookmark
-        self.on_change_bookmark()
-
     def on_activate_title(self, entry):
-        self.bookmark.set_title(self.title_entry.get_text())
-
-    def on_activate_level(self, entry):
-        self.bookmark.set_level(self.level_entry.get_text())
+        model, treeiter = self.selection.get_selected()
+        self.bookmark_store[treeiter][0] = self.title_entry.get_text()
 
     def on_activate_page(self, entry):
-        self.bookmark.set_page(self.page_entry.get_text())
-
-
-class PdfTk:
-    def __init__(self, filename):
-        self.filename = filename
-        self.metadata_pre = []
-        self.metadata_post = []
-        self.bookmarks = []
-        temp_data = enumerate(subprocess.check_output(["pdftk", filename, "dump_data"], \
-                stderr=subprocess.STDOUT).splitlines())
-        found_number_of_pages = False
-
-        for _, line_bytes in temp_data:
-            line = line_bytes.decode('UTF-8')
-            if line == "BookmarkBegin":
-                bookmark = [ temp_data.__next__()[1].decode('UTF-8') ]
-                bookmark.append(temp_data.__next__()[1].decode('UTF-8'))
-                bookmark.append(temp_data.__next__()[1].decode('UTF-8'))
-                self.bookmarks.append(Bookmark(bookmark))
-            else:
-                if found_number_of_pages:
-                    self.metadata_pre.append(line)
-                else:
-                    self.metadata_post.append(line)
-            if line.find("NumberOfPages:") != -1:
-                found_number_of_pages = True
-
-    def save_as(self, new_filename):
-        process = subprocess.Popen(["pdftk", self.filename, "update_info_utf8", "-", "output", new_filename],\
-                stdin=subprocess.PIPE)
-        add_str = lambda a,b: a + "\n" + b
-        full_metadata = reduce(add_str, self.metadata_pre, "") + \
-                reduce(add_str, map(lambda bk: bk.to_string(), self.bookmarks), "") + \
-                reduce(add_str, self.metadata_post, "")
-        process.communicate(input=full_metadata.encode('UTF-8'))
-
-        
+        model, treeiter = self.selection.get_selected()
+        self.bookmark_store[treeiter][1] = self.page_entry.get_text()
 
 class ButtonWithIcon(Gtk.Button):
     def __init__(self, label_text, icon_name):
@@ -176,29 +188,42 @@ class MainWindow(Gtk.Window):
         self.button_save = ButtonWithIcon("Save", "document-save")
         self.button_save.connect("clicked", self.on_save_clicked)
 
+        self.button_add = ButtonWithIcon("New bookmark", "add")
+        self.button_add.connect("clicked", self.on_add_clicked)
+
         self.paned = Gtk.Paned()
         self.paned.set_orientation(Gtk.Orientation.HORIZONTAL)
 
-        self.bookmark_list: Gtk.ListBox = Gtk.ListBox()
-        self.bookmark_list.connect("row-selected", self.on_select_row)
-        self.paned.add1(self.bookmark_list)
+        self.bookmark_view: Gtk.TreeView = Gtk.TreeView()
+        self.bookmark_view.set_reorderable(True)
+        renderer = Gtk.CellRendererText()
+        column = Gtk.TreeViewColumn("Title", renderer, text=0)
+        self.bookmark_view.append_column(column)
+
+        self.paned.add1(self.bookmark_view)
         
-        self.bookmark_edit_box = BookmarkEditBox(None)
+        self.bookmark_edit_box = BookmarkEditBox(None, self.bookmark_view)
         self.paned.add2(self.bookmark_edit_box)
 
         self.empty_label = Gtk.Label(label="No PDF file loaded")
         self.add(self.empty_label)
 
-        self.update_bookmark_list()
         self.show_all()
     
     def on_pdf_first_loaded(self):
         self.remove(self.empty_label)
         self.add(self.paned)
+        self.header_bar.pack_start(self.button_add)
         self.header_bar.pack_start(self.button_save)
         self.header_bar.pack_start(self.button_save_as)
-        self.show_all()
         self.pdf_loaded = True
+    
+    def on_pdf_loaded(self):
+        self.bookmark_view.set_model(self.pdftk.bookmark_store)
+        self.bookmark_edit_box.set_store(self.pdftk.bookmark_store)
+        self.bookmark_view.show_all()
+        self.show_all()
+
 
     def on_open_clicked(self, widget):
         dialog = Gtk.FileChooserDialog(
@@ -219,7 +244,7 @@ class MainWindow(Gtk.Window):
 
         if not self.pdf_loaded:
             self.on_pdf_first_loaded()
-        self.update_bookmark_list()
+        self.on_pdf_loaded()
         dialog.destroy()
 
     def on_save_as_clicked(self, widget):
@@ -242,22 +267,11 @@ class MainWindow(Gtk.Window):
         dialog.destroy()
 
     def on_save_clicked(self, widget):
-        self.pdftk.save_as(self.pdftk.get_filename())
-        dialog.destroy()
+        self.pdftk.save_as(self.pdftk.filename)
 
-    def update_bookmark_list(self):
-        for child in self.bookmark_list.get_children():
-            self.bookmark_list.remove(child)
-        if self.pdftk is not None:
-            for bookmark in self.pdftk.bookmarks:
-                row = BookmarkListRow(bookmark)
-                self.bookmark_list.add(row)
-
-        self.bookmark_edit_box.change_bookmark(None)
-        self.bookmark_list.show_all()
-
-    def on_select_row(self, box, row):
-        self.bookmark_edit_box.change_bookmark(row.bookmark)
+    def on_add_clicked(self, widget):
+        self.pdftk.bookmark_store.append(None, ("<NEW_BOOKMARK>", 1))
+        self.bookmark_view.show_all()
              
     def add_filters(self, dialog):
         filter_pdf = Gtk.FileFilter()
