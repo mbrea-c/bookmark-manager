db669a0bab1b521ac681c0cd103881ba5fdcdcff
Manuel Brea <m.brea.carreras@gmail.com>
2021-04-08 17:35:37 +0100

    Reorganized for packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pdf-bookmark-manager',\
+        version='0.9',\
+        description='Simple GTK+ app for creating or editing pdf bookmarks/table of contents, with optional zathura integration',\
+        url='https://github.com/mbrea-c/pdf-bookmark-manager',\
+        author='Manuel Brea Carreras',\
+        author_email='m.brea.carreras@gmail.com',\
+        license='MIT',\
+        packages=['pdf-bookmark-manager'],\
+        zip_safe=False)
